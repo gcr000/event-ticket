85d8e97dcf596e35d50095d35d3a459c950226fd
giancarlouzzo <giancarlo.uzzo@rstore.it>
2024-05-21 18:45:52 +0200

    - fix QrCode 5 centered

--- resources/views/emails/booking_confirmation.blade.php
+++ resources/views/emails/booking_confirmation.blade.php
@@ -12,8 +12,6 @@
     <!-- CSS Reset : BEGIN -->
     <style>
 
-        /* What it does: Remove spaces around the email design added by some email clients. */
-        /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
         html,
         body {
             margin: 0 auto !important;
@@ -23,25 +21,21 @@
             background: #f1f1f1;
         }
 
-        /* What it does: Stops email clients resizing small text. */
         * {
             -ms-text-size-adjust: 100%;
             -webkit-text-size-adjust: 100%;
         }
 
-        /* What it does: Centers email on Android 4.4 */
         div[style*="margin: 16px 0"] {
             margin: 0 !important;
         }
 
-        /* What it does: Stops Outlook from adding extra spacing to tables. */
         table,
         td {
             mso-table-lspace: 0pt !important;
             mso-table-rspace: 0pt !important;
         }
 
-        /* What it does: Fixes webkit padding issue. */
         table {
             border-spacing: 0 !important;
             border-collapse: collapse !important;
@@ -49,17 +43,14 @@
             margin: 0 auto !important;
         }
 
-        /* What it does: Uses a better rendering method when resizing images in IE. */
         img {
             -ms-interpolation-mode:bicubic;
         }
 
-        /* What it does: Prevents Windows 10 Mail from underlining links despite inline CSS. Styles for underlined links should be inline. */
         a {
             text-decoration: none;
         }
 
-        /* What it does: A work-around for email clients meddling in triggered links. */
         *[x-apple-data-detectors],  /* iOS */
         .unstyle-auto-detected-links *,
         .aBn {
@@ -73,50 +64,34 @@
             line-height: inherit !important;
         }
 
-        /* What it does: Prevents Gmail from displaying a download button on large, non-linked images. */
         .a6S {
             display: none !important;
             opacity: 0.01 !important;
         }
 
-        /* What it does: Prevents Gmail from changing the text color in conversation threads. */
         .im {
             color: inherit !important;
         }
 
-        /* If the above doesn't work, add a .g-img class to any image in question. */
         img.g-img + div {
             display: none !important;
         }
 
-        /* What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89  */
-        /* Create one of these media queries for each additional viewport size you'd like to fix */
-
-        /* iPhone 4, 4S, 5, 5S, 5C, and 5SE */
         @media only screen and (min-device-width: 320px) and (max-device-width: 374px) {
             u ~ div .email-container {
                 min-width: 320px !important;
             }
         }
-        /* iPhone 6, 6S, 7, 8, and X */
         @media only screen and (min-device-width: 375px) and (max-device-width: 413px) {
             u ~ div .email-container {
                 min-width: 375px !important;
             }
         }
-        /* iPhone 6+, 7+, and 8+ */
         @media only screen and (min-device-width: 414px) {
             u ~ div .email-container {
                 min-width: 414px !important;
             }
         }
-
-    </style>
-
-    <!-- CSS Reset : END -->
-
-    <!-- Progressive Enhancements : BEGIN -->
-    <style>
 
         .primary{
             background: #30e3ca;
@@ -381,14 +356,8 @@
             @foreach($content['codes'] as $k => $code)
                 <tr>
                     <td valign="middle" class="hero bg_white" style="text-align: center; padding: 0 2.5em;">
-                        <div class="qrcode_img">
-                            {{--@php($from = [255, 0, 0])
-                            @php($to = [0, 0, 255])
-                            <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={{$code['phone_number']}}" alt="">
-                            <h3>
-                                <span style="color: #871179">{{$code['name']}}</span> - <span style="color: #370EC0">{{$code['phone_number']}}</span>
-                            </h3> <br>--}}
-                            <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={{$code['phone_number']}}" alt="">
+                        <div class="qrcode_img" style="text-align: center;!important;">
+                            <img style="display: block; margin: 0 auto;" src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={{$code['phone_number']}}" alt="">
                         </div>
                     </td>
                 </tr>
@@ -402,14 +371,6 @@
                     </td>
                 </tr>
             @endforeach
-           {{-- <tr>
-                <td valign="middle" class="hero bg_white" style="">
-                    <div id="qrcode_img">
-                        {!! QrCode::style('round')->size(400)->generate($content['code']); !!} <br>
-                        <h3>Mostra questo QrCode all'ingresso per accedere all'evento</h3>
-                    </div>
-                </td>
-            </tr>--}}
         </table>
         <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
             <tr>
